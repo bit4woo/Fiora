6bbdd0945455923ff3a1fcf7f9611b5c1718da6a
bit4woo <bit4woo@163.com>
2021-11-16 17:36:40 +0800

    Update HttpUtil.java

--- src/httpbase/HttpUtil.java
+++ src/httpbase/HttpUtil.java
@@ -1,6 +1,5 @@
 package httpbase;
 
-import java.util.List;
 import java.util.Map;
 
 import com.github.kevinsawicki.http.HttpRequest;
@@ -9,6 +8,8 @@ import com.github.kevinsawicki.http.HttpRequest;
 public class HttpUtil {
 	public static void main(String[] args) {
 		//request("get","https://rosetta.garenanow.com/transify/1366?lang=1",null,null);
+		makeRequest("get","https://rosetta.garenanow.com/transify/1366?lang=1",null,null,"10.12.78.221",8899,
+				"f81a5bb40b9744129dae8bd0efbb9484","5e47a0311f784eb58c2ccb6ecae9d675");
 	}
 	
 	/**
@@ -20,7 +21,8 @@ public class HttpUtil {
 	 * @param proxy 使用的代理服务器 127.0.0.1:8080
 	 * @param proxyBasic 代理服务器的账号密码 username:password
 	 */
-	public static byte[] makeRequest(String method,String url,Map<String, String> headers,byte[] body,String proxy,String proxyBasic){
+	public static byte[] makeRequest(String method,String url,Map<String, String> headers,byte[] body,String proxyHost,
+			int proxyPort,String proxyUsername,String proxyPassword){
 		HttpRequest request;
 		if (url == null) {
 			return null;
@@ -34,6 +36,14 @@ public class HttpUtil {
 		
 		request = new HttpRequest(url,method);
 		
+		if (proxyHost !=null && proxyPort >0) {
+			request.useProxy(proxyHost, proxyPort);
+		}
+		
+		if (proxyUsername != null && proxyPassword != null) {
+			request.proxyBasic(proxyUsername, proxyPassword);
+		}
+		
 		if (headers != null) {
 			request.headers(headers);
 		}
@@ -41,9 +51,9 @@ public class HttpUtil {
 			request.send(body);
 		}
 		
-		System.out.println(request.headers());
-		System.out.println(request.bytes());
-		System.out.println(request.body());
+		//System.out.println(request.headers());
+		//System.out.println(request.bytes());
+		//System.out.println(request.body());
 		return request.bytes();
 		
 	};
